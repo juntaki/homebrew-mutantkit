45b40af5f2b645c70a892e4bd33a42b3bc8c0aa9
juntaki <me@juntaki.com>
2026-08-15 18:29:18 +0900

    mutantkit 0.1.0

--- Formula/mutantkit.rb
+++ Formula/mutantkit.rb
@@ -1,9 +1,9 @@
 class Mutantkit < Formula
   desc "Trustworthy mutation testing for Swift and Apple platforms"
   homepage "https://github.com/juntaki/mutantkit"
-  url "https://github.com/juntaki/mutantkit/releases/download/v0.1.0-alpha.1/mutantkit-macos-arm64.tar.gz"
-  sha256 "32e041eff48af329edea1fd69b49339382c5890b7041556fae2af4f0022782d7"
-  version "0.1.0-alpha.1"
+  url "https://github.com/juntaki/mutantkit/releases/download/v0.1.0/mutantkit-macos-arm64.tar.gz"
+  sha256 "27226988ef5e9a58ed93904fd767b341976a432fe1bebcc6526c1cabade2cd3e"
+  version "0.1.0"
   license "Apache-2.0"
 
   depends_on arch: :arm64
